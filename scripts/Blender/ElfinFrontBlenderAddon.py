b40d1866855a55aaee30b431d4c0c3a4874c9cd1
Joy Yeh <joyyeh.tw@gmail.com>
2018-06-03 18:54:02 +0900

    Implemented obj preprocess in blender

--- scripts/Blender/ElfinFrontBlenderAddon.py
+++ scripts/Blender/ElfinFrontBlenderAddon.py
@@ -1,9 +1,7 @@
 import bpy
-from bpy.types import Operator
-from bpy.types import Panel
 from bpy.props import *
 
-bl_info = {'name': 'Elfin Front', 'category': 'Object'}
+bl_info = {'name': 'Elfin Front', 'category': 'Elfin'}
 
 # Addon design notes
 #   * Each separate object is a separate chain
@@ -15,61 +13,50 @@ bl_info = {'name': 'Elfin Front', 'category': 'Object'}
 #   x Unit conversion: 1 blender unit is 10 A, or 1 nm
 #       x In other words 1 unit in blender is 10 pymol units
 #   * Module avatar generation:
-#       * Singles are pairs exported as OBJ from Pymol
-#           * Pre-process OBJs:
-#               * Load OBJ
-#               * Remove doubles (edit mode)
-#                   bpy.ops.mesh.remove_doubles()
-#               * Decimate to 0.05 ratio
-#                   bpy.ops.object.modifier_add(type='DECIMATE')
-#                   bpy.context.object.modifiers['Decimate'].ratio = 0.05
-#                   bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Decimate')
-#               * Save as new OBJ
+#       * Singles and pairs exported as OBJ from Pymol
+#           * Pre-process OBJs
 #       * How to do avatars in viewport elegantly?
 #
-# bpy.ops.elfin.unload()
-# filename = 'C:\\Users\\Akaoni\\Desktop\\ElfinWork\\elfin\\scripts\\Blender\\ElfinFrontBlenderAddon.py'
-# exec(compile(open(filename).read(), filename, 'exec'))
 
 class ElfinPropertyGroup(bpy.types.PropertyGroup):
-    PpSrcDir = StringProperty(subtype='DIR_PATH')
-    PpDstDir = StringProperty(subtype='DIR_PATH')
+    PpSrcDir = bpy.props.StringProperty(subtype='DIR_PATH', default='//..\\res\\aligned_obj\\')
+    PpDstDir = bpy.props.StringProperty(subtype='DIR_PATH', default='//..\\res\\aligned_blend\\')
+    PpDecimateRatio = bpy.props.FloatProperty(default=0.1, min=0.00, max=1.00)
 
-class ElfinDebugPanel(Panel):
+class ElfinDebugPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_label = 'Debug'
     bl_context = 'objectmode'
     bl_category = 'Elfin'
 
-
     def draw(self, context):
         layout = self.layout
         row = layout.row(align=True)
         col = row.column()
-        col.operator('elfin.unload', text='Unload')
-        col.operator('elfin.delete_faces', text='Delete Faces')
+        col.operator('elfin.reset', text='Reset Properties')
+        col.operator('elfin.delete_faces', text='Delete faces (selection)')
+        col.operator('elfin.preproess_obj', text='Preprocess object (selection)')
 
-class ElfinPreprocessPanel(Panel):
+class ElfinPreprocessPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_label = 'Preprocess'
     bl_context = 'objectmode'
     bl_category = 'Elfin'
 
-
     def draw(self, context):
         scn = context.scene
-
         layout = self.layout
 
         row = layout.row(align=True)
         col = row.column()
         col.prop(scn.ElfinFront, 'PpSrcDir', text='Source')
         col.prop(scn.ElfinFront, 'PpDstDir', text='Destination')
-        col.operator('elfin.preproess_objs', text='Preprocess')
+        col.prop(scn.ElfinFront, 'PpDecimateRatio', text='Decimate Ratio')
+        col.operator('elfin.preproess_all', text='Preprocess')
 
-class ElfinExportPanel(Panel):
+class ElfinExportPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_label = 'Export'
@@ -83,20 +70,7 @@ class ElfinExportPanel(Panel):
         col = row.column()
         col.operator('elfin.export', text='Export to .ei')
 
-class ElfinPreprocessObjsOperator(Operator):
-    bl_idname = 'elfin.preproess_objs'
-    bl_label = 'Preprocess module obj files'
-
-    
-    def execute(self, context):
-        print('Dialog?')
-        return {'FINISHED'}
- 
-    def invoke(self, context, event):
-        self.srcPath = ''
-        return context.window_manager.invoke_props_dialog(self)
-
-class ElfinExportOperator(Operator):
+class ElfinExportOperator(bpy.types.Operator):
     bl_idname = 'elfin.export'
     bl_label = 'Export as Elfin input'
 
@@ -106,25 +80,52 @@ class ElfinExportOperator(Operator):
 
         return {'FINISHED'}
 
-class ElfinUnloadOperator(Operator):
-    bl_idname = 'elfin.unload'
-    bl_label = 'Unload Elfin Front'
+class ElfinPreprocessAllOperator(bpy.types.Operator):
+    bl_idname = 'elfin.preproess_all'
+    bl_label = 'Preprocess module obj files'
 
     def execute(self, context):
-        unregister()
+        print('Unimplemented')
         return {'FINISHED'}
 
-class ElfinDeleteFacesOperator(Operator):
+class ElfinPreprocessObjOperator(bpy.types.Operator):
+    bl_idname = 'elfin.preproess_obj'
+    bl_label = 'Preprocess module object'
+
+    def execute(self, context):
+        for obj in bpy.context.selected_objects:
+            obj.scale = (.1, .1, .1)
+            bpy.context.scene.objects.active = obj
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.customdata_custom_splitnormals_clear()
+            bpy.ops.mesh.remove_doubles()
+            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.modifier_add(type='DECIMATE')
+            bpy.context.object.modifiers['Decimate'].ratio = context.scene.ElfinFront['PpDecimateRatio']
+            bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Decimate')
+        return {'FINISHED'}
+
+class ElfinDeleteFacesOperator(bpy.types.Operator):
     bl_idname = 'elfin.delete_faces'
     bl_label = 'Delete Faces (selected only)'
     
     def execute(self, context):
-        bpy.ops.object.mode_set(mode='OBJECT')
-        for obj in bpy.context.selected_objects:
+        selObjs = bpy.context.selected_objects
+        for obj in selObjs:
             bpy.context.scene.objects.active = obj
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.mesh.delete(type='ONLY_FACE')
             bpy.ops.object.mode_set(mode='OBJECT')
+        return {'FINISHED'}
+
+class ElfinResetOperator(bpy.types.Operator):
+    bl_idname = 'elfin.reset'
+    bl_label = 'Reset Elfin Front properties'
+
+    def execute(self, context):
+        scn = context.scene
+        for k in scn.ElfinFront.keys():
+            scn.ElfinFront.property_unset(k)
         return {'FINISHED'}
 
 def register():
