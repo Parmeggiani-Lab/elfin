48e1f0835bc79816a95383252d342809ff5e9c97
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-01 01:11:38 +0900

    Separate BioPython dependency from main utilities

--- pyelfin/utilities.py
+++ pyelfin/utilities.py
@@ -1,6 +1,5 @@
 import inspect, os, sys, code, traceback
 
-import Bio.PDB
 import numpy as np
 
 import json
@@ -79,35 +78,6 @@ def gen_pymol_txm(rot, tran):
   rotTpTran = np.append(rotTp, np.transpose([tran]), axis=1)
   pymolRotMat = np.append(rotTpTran, [[0,0,0,1]], axis=0)
   return '[' + ', '.join(map(str, pymolRotMat.ravel())) + ']'
-
-def strip_residues(pdb, chain_ids=None):
-  """
-  Returns returns residues removed from a PDB.
-
-  Args:
-  - pdb - Bio.PDB.Structure.Structure.
-  - chain_ids - strip residues from these specific chain_ids only.
-
-  Returns:
-  - residues - a list of Bio.PDB.Residue.Residue.
-  """
-  residues = []
-  for model in pdb:
-    for chain in model:
-      if chain_ids == None or chain.id in chain_ids:
-        tmp = list(chain.child_list)
-        for r in tmp:
-          chain.detach_child(r.id)
-        residues += tmp
-  return residues
-
-def get_chain(struct, chain_id='A'):
-  """Returns a specific chain from a Bio.PDB.Structure.Structure."""
-  return struct.child_list[0].child_dict[chain_id]
-
-def get_chains(struct):
-  """Returns all chains of a Bio.PDB.Structure.Structure."""
-  return struct.child_list[0].child_list;
 
 def get_residue_count(pdb):
   """Returns the residue count of a Bio.PDB.Structure.Structure."""
@@ -346,65 +316,6 @@ def read_json(read_path):
   """
   with open(read_path, 'r') as file:
     return json.load(file)
-
-def read_pdb(
-    read_path,
-    pdb_name=None,
-    permissive=0
-):
-  """
-  Reads a PDB file.
-
-  Args:
-  - read_path - PDB string file path to read from.
-  - pdb_name - a string to set as the name of the Bio.PDB.Structure.Structure.
-  - permissive - sets whether the Bio.PDB.PDBParser is permissive.
-
-  Returns:
-  - structure - Bio.PDB.Structure.Structure.
-  """
-  if pdb_name == None:
-    pdb_name = read_path.split('/')[-1].replace('.', '_')
-  parser = Bio.PDB.PDBParser(permissive)
-  structure = parser.get_structure(pdb_name, read_path)
-  return structure
-
-def save_cif(**kwargs):
-  """
-  Saves a Bio.PDB.Structure.Structure as a CIF file. Does not automatically
-  append .cif extension.
-
-  Args:
-  - struct - Bio.PDB.Structure.Structure to be saved.
-  - save_path - CIF string file path.
-  """
-  struct = kwargs.pop('struct')
-  save_path = kwargs.pop('save_path')
-
-  with open(save_path, 'w') as file:
-    io = Bio.PDB.mmcifio.MMCIFIO()
-    io.set_structure(struct)
-    io.save(file)
-    # Temporary fix for CIF files not getting parsed properly by Rosetta: add
-    # a dummy section at the end. ("Note that the final table in the cif file
-    # may not be recognized - adding a dummy entry (like `_citation.title
-    # ""`) to the end of the file may help.")
-    file.writelines('_citation.title  "Elfin"')
-
-def save_pdb(**kwargs):
-  """
-  Saves a Bio.PDB.Structure.Structure as a PDB file.
-
-  Args:
-  - struct - Bio.PDB.Structure.Structure to be saved.
-  - save_path - string file path.
-  """
-  struct = kwargs.pop('struct')
-  save_path = kwargs.pop('save_path')
-
-  io = Bio.PDB.PDBIO()
-  io.set_structure(struct)
-  io.save(save_path)
 
 def make_dir(dir):
   """
