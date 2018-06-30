f1313ca1e0df43e3def5fa9892d996867faf33cd
Joy Yeh <joyyeh.tw@gmail.com>
2018-06-30 17:57:42 +0900

    Renamed xdb stat script

---
+++ pyelfin/stat_xdb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from utilities import *
+
+def main():
+	(avg_d, min_d, max_d) = com_dist_info(read_json('resources/xDB.json'))
+	print('Distances avg: {}, min: {}, max: {}'.format(avgD, minD, maxD))
+
+if __name__ =='__main__': 
+  safe_exec(main)
