d933ebf977d22174b59316613d0d7d924c86eb01
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-02 22:32:01 +0900

    Fixed gitignore accidentally ignoring template.py

---
+++ pelfin/template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import argparse, sys
+from utilities import *
+
+def main():
+  ap = argparse.ArgumentParser(description='Template Elfin Python script')
+  ap.add_argument('input') # Absence of dash denotes mandatory argument
+  args = ap.parse_args()
+
+if __name__ == '__main__':
+  safe_exec(main)
