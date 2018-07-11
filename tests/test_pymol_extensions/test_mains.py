d2a5d0bf2d877b2c012b861aab53daca965c5368
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-12 00:45:11 +0900

    Fixed pytest for python2

--- tests/test_pymol_extensions/test_mains.py
+++ tests/test_pymol_extensions/test_mains.py
@@ -3,9 +3,9 @@ import importlib
 
 from functools import partial
 
-import helper
+from tests import helper
 
-test_package_name = 'scripts.pymol_extensions'
+test_package_name = 'pymol_extensions'
 script_main_test = partial(helper._test_script_main, package_name=test_package_name)
 non_executable_test = partial(helper._test_non_executable, package_name=test_package_name)
 
