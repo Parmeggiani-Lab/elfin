b5b9b80b4c6b31f129801bc68a87e13a544257d0
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-12 00:45:11 +0900

    Fixed pytest for python2

--- tests/pymol_extensions/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-
-curr_dir = os.path.dirname(__file__)
-sys.path.append(os.path.join(curr_dir, os.pardir)) # for _test_utilities
-sys.path.append(os.path.join(curr_dir, os.pardir, os.pardir, 'scripts', 'pymol_extensions')) # for test scripts
