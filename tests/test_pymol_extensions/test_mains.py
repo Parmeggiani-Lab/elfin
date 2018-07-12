044eff029bf77c4908843f472ef46ebfe00d3206
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-12 19:35:21 +0900

    Wrote out PyMol frame shifting visualization tests, tested them, put them in proper directory, and added to pytest. Autoloads in Pymol on startup. See pymolrc.pml

--- tests/test_pymol_extensions/test_mains.py
+++ tests/test_pymol_extensions/test_mains.py
@@ -5,7 +5,7 @@ from functools import partial
 
 from tests import helper
 
-test_package_name = 'pymol_extensions'
+test_package_name = 'extensions'
 script_main_test = partial(helper._test_script_main, package_name=test_package_name)
 non_executable_test = partial(helper._test_non_executable, package_name=test_package_name)
 
@@ -15,3 +15,7 @@ def test_mains():
   non_executable_test('load_all_extensions')
   non_executable_test('draw_lines')
   non_executable_test('batch_convert')
+  non_executable_test('extrude_hub_at_single_c')
+  non_executable_test('extrude_hub_at_single_n')
+  non_executable_test('extrude_single_at_single_c')
+  non_executable_test('extrude_single_at_single_n')
