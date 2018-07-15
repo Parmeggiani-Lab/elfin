60b58e0b1f8e89e405a45a9c2788ab8e904bb201
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-15 19:35:04 +0900

    Switched back to 4-space indent. Upped pylint score on utilities.py

--- pelfin/hubinfo_convert.py
+++ pelfin/hubinfo_convert.py
@@ -6,23 +6,23 @@ from utilities import *
 
 
 def parse_args(args):
-  parser = argparse.ArgumentParser(description='Converts hub info metadata from csv to json.')
-  parser.add_argument('input') # Absence of dash denotes mandatory argument
-  return parser.parse_args(args)
+    parser = argparse.ArgumentParser(description='Converts hub info metadata from csv to json.')
+    parser.add_argument('input') # Absence of dash denotes mandatory argument
+    return parser.parse_args(args)
 
 def main(test_args=None):
-  args = parse_args(sys.argv[1:] if test_args is None else test_args)
+    args = parse_args(sys.argv[1:] if test_args is None else test_args)
 
-  csv_data = read_csv(args.input)
-  new_data = OrderedDict({})
-  for i in range(int(len(csv_data)/2)):
-    row = csv_data[int(2*i)]
-    chains = csv_data[int(2*i+1)]
-    component_info = OrderedDict({})
-    for i in range(int(len(chains)/2)):
-      component_info[chains[int(2*i)]] = { 'single_name': chains[int(2*i+1)], 'c_free': row[4]=='C_free', 'n_free': row[3]=='N_free' }
-    new_data[row[0].replace('.pdb', '')] = OrderedDict({ 'oligomer_type': row[1], 'symmetric': row[2] == 'symmetric', 'component_info': component_info})
-  json.dump(new_data, open(args.input.replace('.csv', '') + '.json', 'w'), separators=(',',':'), ensure_ascii=False, indent=4)
+    csv_data = read_csv(args.input)
+    new_data = OrderedDict({})
+    for i in range(int(len(csv_data)/2)):
+        row = csv_data[int(2*i)]
+        chains = csv_data[int(2*i+1)]
+        component_info = OrderedDict({})
+        for i in range(int(len(chains)/2)):
+            component_info[chains[int(2*i)]] = { 'single_name': chains[int(2*i+1)], 'c_free': row[4]=='C_free', 'n_free': row[3]=='N_free' }
+        new_data[row[0].replace('.pdb', '')] = OrderedDict({ 'oligomer_type': row[1], 'symmetric': row[2] == 'symmetric', 'component_info': component_info})
+    json.dump(new_data, open(args.input.replace('.csv', '') + '.json', 'w'), separators=(',',':'), ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
-  safe_exec(main)
+    safe_exec(main)
