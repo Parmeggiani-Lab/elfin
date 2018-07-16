3f2e5af418094e7d1230d11206c6c812ef81f455
Joy Yeh <joyyeh.tw@gmail.com>
2018-07-16 12:47:21 +0900

    Separated debug code to it's own file. Implemented better module selector naming so that extrusion source and destination chain IDs are included and parsed properly. So far only done on Extrude CTerm

--- pelfin/hubinfo_convert.py
+++ pelfin/hubinfo_convert.py
@@ -18,10 +18,10 @@ def main(test_args=None):
     for i in range(int(len(csv_data)/2)):
         row = csv_data[int(2*i)]
         chains = csv_data[int(2*i+1)]
-        component_info = OrderedDict({})
+        component_data = OrderedDict({})
         for i in range(int(len(chains)/2)):
-            component_info[chains[int(2*i)]] = { 'single_name': chains[int(2*i+1)], 'c_free': row[4]=='C_free', 'n_free': row[3]=='N_free' }
-        new_data[row[0].replace('.pdb', '')] = OrderedDict({ 'oligomer_type': row[1], 'symmetric': row[2] == 'symmetric', 'component_info': component_info})
+            component_data[chains[int(2*i)]] = { 'single_name': chains[int(2*i+1)], 'c_free': row[4]=='C_free', 'n_free': row[3]=='N_free' }
+        new_data[row[0].replace('.pdb', '')] = OrderedDict({ 'oligomer_type': row[1], 'symmetric': row[2] == 'symmetric', 'component_data': component_data})
     json.dump(new_data, open(args.input.replace('.csv', '') + '.json', 'w'), separators=(',',':'), ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
