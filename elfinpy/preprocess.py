7642ef2a2869a48af708d9a5e22ee1ab013bb4b4
Leon <leonobendorf@yahoo.com>
2020-05-01 15:15:01 +0200

    typo correction

--- elfinpy/preprocess.py
+++ elfinpy/preprocess.py
@@ -143,7 +143,7 @@ def parse_args(args):
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(description='Preprocess all raw single and double PDBs');
     parser.add_argument('--input_dir', default='./pdb_raw/')
-    parser.add_argument('--output_dir', default='./ressources/pdb_prepped/')
+    parser.add_argument('--output_dir', default='./resources/pdb_prepped/')
     parser.add_argument('--dry_run', action='store_true')
     return parser.parse_args(args)
 
